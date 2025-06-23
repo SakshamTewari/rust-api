eb8c0f2bc8126f6e83c2190bf9f837d099f2e978
Saksham Tewari <sakshamtewari3@gmail.com>
2025-06-24 00:54:58 +0530

    add: endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
 #[macro_use] extern crate rocket;
 
+use rocket::serde::json::{Value, json};
+use rocket::response::status;
+
 #[get("/")]
-fn hello() {
-    println!("Hello, Rusty APIs");
+fn hello_rustaceans() -> Value {
+    json!("Hello, Rustaceans!")
+}
+
+#[get("/rustaceans")]
+fn get_rustaceans() -> Value {
+    json!([{"id":1, "name":"Saksham"}, {"id":2, "name":"Tewari"}])
+}
+#[get("/rustaceans/<id>")]
+fn view_rustacean(id: i32) -> Value {
+    json!([{"id": id, "name":"Saksham Tewari"}])
+}
+#[post("/rustaceans", format = "json")]
+fn create_rustacean() -> Value {
+    json!([{"id":3, "name":"Marcos"}])
+}
+#[put("/rustaceans/<id>", format = "json")]
+fn update_rustaceans(id: i32) -> Value {
+    json!([{"id":id, "name":"Saksham Tewari"}])
+}
+#[delete("/rustaceans/<_id>")]
+fn delete_rustaceans(_id: i32) -> status::NoContent {
+    status::NoContent
 }
 
 #[rocket::main]
 async fn main(){
-    let _ = rocket::build().mount("/", routes![hello]).launch().await;
+    let _ = rocket::build().mount("/", routes![hello_rustaceans,get_rustaceans, view_rustacean, create_rustacean,update_rustaceans,delete_rustaceans]).launch().await;
 }
